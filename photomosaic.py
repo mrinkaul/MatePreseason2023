72e4879ff081adfc53bd3f0734b5a092f370f12a
mrinkaul <mrin.kaul@gmail.com>
2022-12-29 00:01:00 -0500

    Added full contour processing to images. Only finding tape contours and not full box. Also added couple test images to run code on and it works based on the size factor of the image resizeImage.

--- photomosaic.py
+++ photomosaic.py
@@ -1,20 +1,65 @@
 import cv2
-import matplotlib.pyplot as plt
-
+import imutils
 import utils
 
-image = cv2.imread("resources\\photomosaic1.jpg")
+
+image = cv2.imread("resources\\photomosaic1.JPG")
+#
 resized_image = utils.resizeImage(image, 0.1)
-cv2.imshow("Resized image", resized_image)
-
 gray_image = utils.grayConvert(resized_image)
-cv2.imshow("gray image", gray_image)
+noise_reduced = utils.noiseRemoval(gray_image, 10, 40, 40)
+utils.drawAndPauseImage(noise_reduced, "Noise Reduced")
+canny_edge = utils.CannyEdge(noise_reduced, 30, 200)
+utils.drawAndPauseImage(canny_edge, "Canny Edge")
 
-noise_reduced = utils.noiseRemoval(gray_image)
-cv2.imshow("noise down img", noise_reduced)
 
-canny_edge = utils.CannyEdge(noise_reduced)
-cv2.imshow("edge sharpen", noise_reduced)
+contours = cv2.findContours(canny_edge.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(contours)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:50]
+rectangleContour = None
 
-cv2.waitKey(0)
+countour_number = 0;    `
+for c in contours:
+     perim = cv2.arcLength(c, True)
+     #print("For contour ", countour_number, " perim is ", perim)
+
+     area = cv2.contourArea(c)
+     #print("For contour ", countour_number, " area is ", area)
+
+     epsilon = 0.01*perim;
+
+     approx = cv2.approxPolyDP(c, epsilon, True)
+     #print("For contour ", countour_number, " approx is ", approx)
+
+     type = "unknown"
+     i, j = approx[0][0]
+     if len(approx) == 3:
+         type = 'Triangle'
+     elif len(approx) == 4:
+         type = 'Rectangle'
+     elif len(approx) == 5:
+         type = 'Pentagon'
+     elif 6 < len(approx) < 15:
+         type = 'Eclipse'
+     else:
+         type = 'Circle'
+
+     print("contour ", countour_number, "is type ", type, " perim is ", perim, " area is ", area)
+     if(type=='Rectangle' or type=='Pentagon'):
+        temp_image = cv2.drawContours(resized_image.copy(), [approx], 0, (0, 255, 0), 3)
+        utils.drawAndPauseImage(temp_image, "Temp Contour "+str(countour_number))
+
+
+     countour_number = countour_number + 1
+     if len(approx) == 4:
+         rectangleContour = approx
+         #break
+
+# If we get here, hopefully we found a rectangle
+if rectangleContour.any():
+    cntrim = cv2.drawContours(resized_image.copy(), [rectangleContour], -1, (0, 255, 0), 3)
+    utils.drawAndPauseImage(cntrim, "Final Rectangle")
+else:
+    print('Did not find any rectangle')
+
 
--- utils.py
+++ utils.py
@@ -7,7 +7,17 @@ def grayConvert(img):
     return cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 
 def noiseRemoval(img):
-    return cv2.bilateralFilter(img, 9, 75, 75)
+    return cv2.bilateralFilter(img, 11, 17, 17)
+
+def noiseRemoval(img, d, sigmaColor, sigmaSpace):
+    return cv2.bilateralFilter(img, d, sigmaColor, sigmaSpace)
 
 def CannyEdge (img):
-    return cv2.Canny(img, 250, 255)
+    return cv2.Canny(img, 30, 200)
+
+def CannyEdge (img, lower_t, higher_t):
+    return cv2.Canny(img, lower_t, higher_t)
+
+def drawAndPauseImage(img, title):
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
